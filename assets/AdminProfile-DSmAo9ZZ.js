8024c046a133f58db2650083f1d199e244adaef6
Tanya-Sinha-hub <waxahev451@bflcafe.com>
2024-12-18 14:42:37 +0700

    Updates

--- assets/AdminProfile-DSmAo9ZZ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BixFw_h0.js";const e=()=>r.jsx("div",{children:"AdminProfile"});export{e as default};
---
+++ assets/Page404-BWL5OcFj.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DVMs5Pnf.js";const e=()=>r.jsx(r.Fragment,{children:"404"});export{e as default};
