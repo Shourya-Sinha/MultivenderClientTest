e345bdbb4944717f0af2567294f57094ab58bdd1
Tanya-Sinha-hub <waxahev451@bflcafe.com>
2024-12-18 15:06:24 +0700

    Updates

--- assets/AdminProfile-BtDhR0up.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CG0d5Cul.js";const e=()=>r.jsx("div",{children:"AdminProfile"});export{e as default};
---
+++ assets/men--Uc2zJHN.js
@@ -0,0 +1,1 @@
+const e="/MultivenderClientTest/assets/men-BCE1w3yb.png";export{e as F};
